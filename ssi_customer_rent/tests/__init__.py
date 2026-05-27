e2f718ce12d7dc222b763e5dadd95e233d425699
Andhitia Rama <andhitia.r@gmail.com>
2026-05-28 00:10:30 +0700

    [UPD] ssi_customer_rent
    * Fix xml-deprecated-data-node in data XML files (use <odoo> instead of <odoo><data>)
    * Add unit tests: test_customer_rent_type and test_customer_rent_order
    * Tests cover: master data CRUD, full workflow (draft→confirm→ready→open→done), cancel workflow

---
+++ ssi_customer_rent/tests/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_customer_rent_order
+from . import test_customer_rent_type
